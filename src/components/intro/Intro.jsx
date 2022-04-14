faf8ad1db3f7ca58b3d9afb9ad7368d382cc1775
yeferfer <jefer.1142@gmail.com>
2022-04-14 13:56:34 -0500

    Intro Update

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./intro.scss";
 
 export default function Intro() {
@@ -9,7 +8,18 @@ export default function Intro() {
           <img src="assets/man.png" alt="" />
         </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Yeferson Culma</h1>
+          <h3>
+            <span>Freelance</span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./topbar.scss";
 import { Person, Mail } from "@mui/icons-material";
 
