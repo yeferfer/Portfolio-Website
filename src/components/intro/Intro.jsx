d3694a3d71c610975831db9605a9dc3c7b7671ac
yeferfer <jefer.1142@gmail.com>
2022-04-14 19:38:24 -0500

    Add Work

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,19 @@
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Designer", "Content creator"],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -13,7 +26,7 @@ export default function Intro() {
           <h2>Hi There, I'm</h2>
           <h1>Yeferson Culma</h1>
           <h3>
-            <span>Freelance</span>
+            Freelance <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portfolio">
