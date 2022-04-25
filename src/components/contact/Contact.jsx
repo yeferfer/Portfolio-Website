2ae05cceb3dab8554baab974e92e95590eed7c18
yeferfer <jefer.1142@gmail.com>
2022-04-25 00:07:08 -0500

    Send message Email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,38 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./contact.scss";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
   const [message, setMessage] = useState(false);
+  const form = useRef();
+
+  const valuesEmail = {
+    to_name: "",
+    from_name: "",
+    message: "",
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setMessage(true);
+
+    emailjs
+      .send(
+        "service_02k5eh9",
+        "template_3o1t7bm",
+        valuesEmail,
+        "gx0ao35F7FUBXP7OE"
+      )
+      .then(
+        function (response) {
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
   };
+
   return (
     <div className="contact" id="contact">
       <div className="left">
@@ -15,20 +40,33 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        <form onSubmit={handleSubmit}>
+        <form ref={form} onSubmit={handleSubmit}>
+          <input type="text" placeholder="Name" className="form-Name" />
           <input type="text" placeholder="Email" className="form-Email" />
           <textarea placeholder="Message" className="form-Message"></textarea>
           <button
             type="submit"
             onClick={() => {
-              console.log(document.querySelector(".form-Email"));
+              valuesEmail.to_name = document.querySelector(".form-Name").value;
+              valuesEmail.from_name =
+                document.querySelector(".form-Email").value;
+              valuesEmail.message =
+                document.querySelector(".form-Message").value;
+              document.querySelector(".form-Name").value = "";
               document.querySelector(".form-Email").value = "";
               document.querySelector(".form-Message").value = "";
+              setTimeout(() => {
+                document
+                  .querySelector(".sendEmailText")
+                  .classList.toggle("hidden");
+              }, 3000);
             }}
           >
             Send
           </button>
-          {message && <span>Thanks, I'll reply ASAP 😊</span>}
+          {message && (
+            <span className="sendEmailText">Thanks, I'll reply ASAP 😊</span>
+          )}
         </form>
       </div>
     </div>
